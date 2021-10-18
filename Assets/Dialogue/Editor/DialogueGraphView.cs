b3ad8f13ca06eedd522a4f46238d44cb351290f5
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-18 23:23:23 +0800

    Generate dialogue entry point node

--- Assets/Dialogue/Editor/DialogueGraphView.cs
+++ Assets/Dialogue/Editor/DialogueGraphView.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class DialogueGraphView : GraphView
@@ -8,5 +10,27 @@ public class DialogueGraphView : GraphView
         this.AddManipulator(new ContentDragger());
         this.AddManipulator(new SelectionDragger());
         this.AddManipulator(new RectangleSelector());
+        
+        AddElement(GenerateEntryPointNode());
+    }
+    
+    /// <summary>
+    /// Generate dialogue entry point node in the graph view
+    /// </summary>
+    /// <returns>Entry Dialogue Node</returns>
+    private DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "Start", // The name of the node
+            GUID = Guid.NewGuid().ToString(),
+            dialogueText = "Entry point",
+            entryPoint = true
+        };
+        
+        // Set the node position
+        node.SetPosition(new Rect(100, 200, 100, 150));
+
+        return node;
     }
 }
