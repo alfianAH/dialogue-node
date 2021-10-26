bd89eeebeb10b66302fe5ee71d7eedc78b999339
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-26 22:40:55 +0800

    Add manipulator on graph view

--- Assets/Editor/BehaviourTreeView.cs
+++ Assets/Editor/BehaviourTreeView.cs
@@ -11,8 +11,14 @@ public class BehaviourTreeView: GraphView
     
     public BehaviourTreeView()
     {
+        // Add grid
         Insert(0, new GridBackground());
         
+        this.AddManipulator(new ContentZoomer());
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+
         var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/BehaviourTreeEditor.uss");
         styleSheets.Add(styleSheet);
     }
