0c02d4cfdc19c7bee00fa66bb417df569be6c42a
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-26 20:44:21 +0800

    Add behaviour tree

---
+++ Assets/Scripts/CustomNode/BehaviourTree.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu()]
+public class BehaviourTree : ScriptableObject
+{
+    public Node rootNode;
+    public Node.State treeState = Node.State.Running;
+
+    public Node.State Update()
+    {
+        return rootNode.Update();
+    }
+}
