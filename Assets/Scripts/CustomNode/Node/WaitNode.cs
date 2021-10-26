602bab99935cf6adf7e9308fa5204c0cd49568a7
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-26 22:00:53 +0800

    Add wait node script

---
+++ Assets/Scripts/CustomNode/Node/WaitNode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class WaitNode: ActionNode
+{
+    public float duration = 1;
+    private float startTime;
+    
+    protected override void OnStart()
+    {
+        startTime = Time.time;
+    }
+
+    protected override void OnStop()
+    {
+        
+    }
+
+    protected override State OnUpdate()
+    {
+        // If done waiting, ...
+        if (Time.time - startTime > duration)
+        {
+            return State.Success; // Return success
+        }
+
+        return State.Running;
+    }
+}
