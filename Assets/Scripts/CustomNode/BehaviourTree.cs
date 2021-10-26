6c48889d4b2911d95bc2646f3cd28f711c0b9338
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-26 21:17:14 +0800

    Add comments in behaviour tree

--- Assets/Scripts/CustomNode/BehaviourTree.cs
+++ Assets/Scripts/CustomNode/BehaviourTree.cs
@@ -8,8 +8,10 @@ public class BehaviourTree : ScriptableObject
 
     public Node.State Update()
     {
+        // If root node's state is running, ...
         if(rootNode.state == Node.State.Running)
         {
+            // Update the tree state
             treeState = rootNode.Update();
         }
 
