f9c9efa55d5bddb6bb7ac339f9374ba2281d6718
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-26 22:36:05 +0800

    Add UI Builder with graph view

---
+++ Assets/Editor/BehaviourTreeView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+public class BehaviourTreeView: GraphView
+{
+    public new class UxmlFactory : UxmlFactory<BehaviourTreeView, UxmlTraits>
+    {
+        
+    }
+    
+    public BehaviourTreeView()
+    {
+        Insert(0, new GridBackground());
+        
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/BehaviourTreeEditor.uss");
+        styleSheets.Add(styleSheet);
+    }
+}
