17e36e3979244b35e07a5c832ee6fe43af0e0ba2
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-26 20:40:57 +0800

    Add node

---
+++ Assets/Scripts/CustomNode/Node.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public abstract class Node : ScriptableObject
+{
+    public enum State
+    {
+        Running, Failure, Success
+    }
+
+    public State state = State.Running;
+    public bool started = false;
+
+    public State Update()
+    {
+        // If not started yet, ...
+        if (!started)
+        {
+            // Start it
+            OnStart();
+            started = true;
+        }
+        
+        state = OnUpdate(); // Update the state
+        
+        // If the state is failure or success, ...
+        if (state == State.Failure || state == State.Success)
+        {
+            // Stop it
+            OnStop();
+            started = false;
+        }
+
+        return state;
+    }
+
+    protected abstract void OnStart();
+    protected abstract void OnStop();
+    protected abstract State OnUpdate();
+}
