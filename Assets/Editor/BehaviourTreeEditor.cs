7d532524953afcc4c3f885f1df3c6da1dd17dbd7
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-27 15:33:53 +0800

    Save node position

--- Assets/Editor/BehaviourTreeEditor.cs
+++ Assets/Editor/BehaviourTreeEditor.cs
@@ -33,6 +33,8 @@ public class BehaviourTreeEditor : EditorWindow
 
         treeView = root.Q<BehaviourTreeView>();
         inspectorView = root.Q<InspectorView>();
+        
+        OnSelectionChange();
     }
 
     private void OnSelectionChange()
--- Assets/Editor/NodeView.cs
+++ Assets/Editor/NodeView.cs
@@ -1,10 +1,24 @@
-﻿public class NodeView: UnityEditor.Experimental.GraphView.Node
+﻿using UnityEngine;
+
+public class NodeView: UnityEditor.Experimental.GraphView.Node
 {
     public Node node;
     
     public NodeView(Node node)
     {
         this.node = node;
-        this.title = node.name;
+        title = node.name;
+        viewDataKey = node.guid;
+        
+        // Set position
+        style.left = node.position.x;
+        style.top = node.position.y;
+    }
+
+    public override void SetPosition(Rect newPos)
+    {
+        base.SetPosition(newPos);
+        node.position.x = newPos.xMin;
+        node.position.y = newPos.yMin;
     }
 }
