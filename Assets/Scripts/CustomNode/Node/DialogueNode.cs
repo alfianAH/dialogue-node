993c68d5282f312092217e932dd12dc2be940b59
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-11-03 22:33:37 +0800

    Add dialogue node

---
+++ Assets/Scripts/CustomNode/Node/DialogueNode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DialogueNode: DecoratorNode
+{
+    public string charaName;
+    [TextArea(3, 5)]
+    public string dialogueText;
+    public Sprite characterSprite;
+    
+    protected override void OnStart() { }
+
+    protected override void OnStop() { }
+
+    protected override State OnUpdate()
+    {
+        return State.Running;
+    }
+}
