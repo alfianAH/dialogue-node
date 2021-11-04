8ccaa828ff11d75972043a46847b61d96e9ba623
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-11-05 00:46:43 +0800

    Add choice port in choice node view

--- Assets/Editor/ChoiceNodeView.cs
+++ Assets/Editor/ChoiceNodeView.cs
@@ -22,6 +22,64 @@ public class ChoiceNodeView: INodeView
         Label descriptionLabel = this.Q<Label>("description");
         descriptionLabel.bindingPath = "description";
         descriptionLabel.Bind(new SerializedObject(node));
+        
+        Button addChoiceBtn = this.Q<Button>("add-choice-btn");
+        addChoiceBtn.clicked += () => { AddChoicePort(); };
+    }
+    
+    /// <summary>
+    /// Generate port in the node
+    /// </summary>
+    /// <param name="node">Node</param>
+    /// <param name="portDirection">Port direction</param>
+    /// <param name="capacity">The capacity of the port</param>
+    /// <returns>The instantiated port in the node</returns>
+    private Port GeneratePort(ChoiceNodeView node,
+        Direction portDirection,
+        Port.Capacity capacity = Port.Capacity.Single)
+    {
+        return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float));
+    }
+    
+    /// <summary>
+    /// Add choice port
+    /// </summary>
+    /// <param name="overiddenPortName"></param>
+    private void AddChoicePort(string overiddenPortName = "")
+    {
+        // Generate port
+        var generatedPort = GeneratePort(this, Direction.Output);
+        
+        var outputPortCount = outputContainer.Query("connector").ToList().Count;
+        
+        // Make choice port Name
+        var choicePortName = string.IsNullOrEmpty(overiddenPortName)
+            ? $"Choice {outputPortCount + 1}"
+            : overiddenPortName;
+        
+        // Generate label
+        var choiceLabel = new Label
+        {
+            name = choicePortName,
+        };
+        
+        generatedPort.contentContainer.Add(choiceLabel);
+        outputContainer.Add(generatedPort);
+        RefreshPorts();
+        
+        
+
+        // Add delete choice button
+        // var deleteButton = new Button(() => RemovePort(choiceNode, generatedPort))
+        // {
+        //     text = "X"
+        // };
+        // generatedPort.contentContainer.Add(deleteButton);
+        //
+        // generatedPort.portName = choicePortName;
+        //
+        // // Add generated port to node
+        // choiceNode.outputContainer.Add(generatedPort);
     }
 
     protected override void SetupClasses()
